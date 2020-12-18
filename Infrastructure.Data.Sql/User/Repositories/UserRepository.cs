5cbd9b65c7ed5f951afd28abb147989fed87ea3c
Jay Shi <25982097@qq.com>
2020-12-19 01:57:55 +0800

    Update UserRepository by include hobby

--- Infrastructure.Data.Sql/User/Repositories/UserRepository.cs
+++ Infrastructure.Data.Sql/User/Repositories/UserRepository.cs
@@ -1,6 +1,6 @@
 ﻿using Common.Core.DependencyInjection;
 using Infrastructure.Data.Sql.User.Entities;
-using System;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
 
 namespace Infrastructure.Data.Sql.User.Repositories
@@ -17,7 +17,7 @@ namespace Infrastructure.Data.Sql.User.Repositories
 
         private IQueryable<UserEntity> FindAll()
         {
-            return _context.Users.AsQueryable();
+            return _context.Users.Include(e => e.hobbies).AsQueryable();
         }
 
         public UserEntity FindByGuid(string guid)
