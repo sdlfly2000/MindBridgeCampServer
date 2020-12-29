71d4b0c5c39a18e87164de704786a736fcf80613
Jay Shi <JayShi@eurofins.com>
2020-12-29 16:43:13 +0800

    add interface participantPersistor

---
+++ Domain.Services/LearningRoom/Synchronizors/Persistors/IParticipantPersistor.cs
@@ -0,0 +1,10 @@
+﻿using Domain.LearningRoom;
+using Domain.User;
+
+namespace Domain.Services.LearningRoom.Synchronizors.Persistors
+{
+    public interface IParticipantPersistor
+    {
+        void Update(UserReference participant, RoomReference room);
+    }
+}
