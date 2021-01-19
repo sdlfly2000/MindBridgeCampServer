6c73625a4aa0229f28dfb77a637e0da932d1b947
Jay Shi <25982097@qq.com>
2021-01-19 22:35:18 +0800

    Fix get current datetime

--- Core/DateTimeUtil.cs
+++ Core/DateTimeUtil.cs
@@ -6,7 +6,7 @@ namespace Core
     {
         public static DateTime GetNow()
         {
-            var now = DateTime.Now.AddHours(8);
+            var now = DateTime.Now;
 
             return DateTime.Parse(string.Format("{0}-{1}-{2} {3}:{4}:{5}", now.Year, now.Month, now.Day, now.Hour, now.Minute, now.Second));
         }
