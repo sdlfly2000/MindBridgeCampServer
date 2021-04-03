1a4c18dbf0f5f783175bcaec1eae20d51f272c77
Jay Shi <25982097@qq.com>
2021-04-03 20:57:20 +0800

    Adding ImageEntity

---
+++ Infrastructure.Data.Sql/Image/Entities/ImageEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.Data.Sql.Image.Entities
+{
+    public class ImageEntity
+    {
+        [Key]
+        public string imageId { get; set; }
+
+        public string imageName { get; set; }
+
+        public string imageDirectory { get; set; }
+
+        public int imageStatus { get; set; }
+    }
+}
