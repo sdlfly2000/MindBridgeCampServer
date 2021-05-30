da6839d00eba16dae6ff45336d26ecc8fb78022c
Jay Shi <25982097@qq.com>
2021-05-31 01:13:48 +0800

    Save Image File

--- MindBridgeCampServer/Controllers/NoteController.cs
+++ MindBridgeCampServer/Controllers/NoteController.cs
@@ -1,8 +1,10 @@
 ﻿using Application.Services.Note;
 using Common.Core.LogService;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MindBridgeCampServer.Models;
 using System;
+using SysFile = System.IO.File;
 
 namespace MindBridgeCampServer.Controllers
 {
@@ -32,6 +34,31 @@ namespace MindBridgeCampServer.Controllers
             }
 
             return Ok("Create Note Successfully.");
+        }
+
+        [HttpPost]
+        [LogService]
+        public IActionResult UploadImage(IFormFile image)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Upload Image Error.");
+            }
+
+            var message = "Image Name: " + image.Name + Environment.NewLine;
+            message += "Image FileName: " + image.FileName + Environment.NewLine;
+            message += "Image ContentType: " + image.ContentType + Environment.NewLine;
+            message += "Image ContentDisposition: " + image.ContentDisposition + Environment.NewLine;
+
+            using (var file = SysFile.Create("image.jpg"))
+            {
+                image.OpenReadStream().CopyTo(file);
+                file.Close();
+            }
+
+            LogService.Info<NoteController>(message);            
+
+            return Ok("Upload Image.");
         }
     }
 }
